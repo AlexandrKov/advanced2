34ef0ae4e4b093572888bbb5c57e2e33f88fa9e2
AlexandrKov <mulah1988@mail.ru>
2023-10-21 21:07:27 +0300

    first commit

---
+++ src/__test__/test.js
@@ -0,0 +1,55 @@
+import extraAttack from '../index';
+
+test('test function extraAttack', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  const res = extraAttack(character);
+  expect(res).toEqual(result);
+});
+
+test('test function extraAttack without special', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+  };
+  const result = [];
+  const res = extraAttack(character);
+  expect(res).toEqual(result);
+});
